a5fa7085baedc66e429868929723ca1a6371765e
Romain <rom.dev@live.fr>
2020-04-29 18:06:22 +0200

    07.11 - Authentification : repérer les erreurs avec la class AuthentificationUtils [DONE]

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
@@ -11,10 +13,19 @@ class AccountController extends AbstractController
      * Permet d'afficher et de gérer le formulaire de contact
      *
      * @Route("/login", name="account_login")
+     * @param  AuthenticationUtils $utils
+     * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig',
+        [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
     }
 
     /**
