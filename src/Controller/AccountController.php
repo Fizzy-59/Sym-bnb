e957fb34efd6d218b6cb42250a30ded6af6ae86a
Romain <rom.dev@live.fr>
2020-04-30 09:17:51 +0200

    07.14 - Enrengistrer les données du formulaire d’inscription [DONE]

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
@@ -44,12 +48,34 @@ class AccountController extends AbstractController
      * Permet d'afficher le formulaire d'inscription
      *
      * @Route("/register", name="account_register")
+     * @param  Request $request
+     * @param  ObjectManager $manager
+     * @param  UserPasswordEncoderInterface $encoder
+     * @return Response
      */
-    public function register()
+    public function register(Request $request,  EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
 
         $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $hash = $encoder->encodePassword($user, $user->getHash());
+            $user->setHash($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé."
+            );
+
+            return $this->redirectToRoute('account_login');
+        }
 
         return $this->render('account/registration.html.twig',
         [
